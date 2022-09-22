ab8eb0b6c150e928584b354e9d24a21cf08003a2
Sinter <sintercver@163.com>
2022-09-22 18:10:32 +0800

    fixed the array_sum

--- tests/array_sum.cu
+++ tests/array_sum.cu
@@ -1,61 +1,62 @@
 #include <cstddef>
-#include <stdio.h>
 #include <iostream>
+#include <array>
 #include <cuda_runtime.h>
+#include <numeric>
 
+#define threadsPerBlock 256
 
 __global__ void arraySum(const float* a, float *ret, int numElements) {
-  int tid = blockDim.x * blockIdx.x + threadIdx.x;
-  if (tid < numElements) {
-    __shared__ float sData[16];  // 申请共享数据内存
-    sData[tid] = a[tid];
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  int block_id = blockIdx.x;
+  int block_tid = threadIdx.x;
+  __shared__ float sData[threadsPerBlock];  // 申请共享数据内存（每个block中的线程共享）
+  sData[block_tid] = a[tid];
+  __syncthreads();
+  for (int i = threadsPerBlock / 2; i > 0; i /= 2) {
+    if (block_tid < i) {
+      sData[block_tid] = sData[block_tid] + sData[block_tid + i];
+    }
     __syncthreads();
-    for (int i = numElements / 2; i > 0; i /= 2) {
-      if (tid < i) {
-        sData[tid] = sData[tid] + sData[tid + i];
-      }
-      __syncthreads();
-    }
-    if (tid == 0) {
-      ret[0] = sData[0];
-    }
+  }
+  if (block_tid == 0) {
+    ret[block_id] = sData[0];
   }
 }
 
 int main(int argc, char **argv) {
 
-  // std::array<float, 16> a;
-  // for (auto &i : a) {
-  //   i *= (i + 1);
-  // }
+  // 数据数量需要符合2^n，不够需要补0
+  constexpr int numElements = 5120;
 
-  int numElements = 16;
+  const int blockPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;  // block数量
+
+  std::array<float, numElements> data;
+  for (int i = 0; i < data.size(); i ++) {
+    data[i] = i;
+  }
+  // for (auto i : data) {
+  //   std::cout << i << ", ";
+  // }
+  // std::cout << std::endl;
+
   size_t size = numElements * sizeof(float);
 
-  float *h_data = (float *)malloc(size);;
-  float *h_ret = (float *)malloc(size); // result on the host
-  float* d_data = nullptr;  // data of device
+  std::array<float, blockPerGrid> ret; // result on the host
+  float *d_data = nullptr;  // data of device
   float *d_ret = nullptr; // result on the device
-
-  for (int i = 0; i < numElements; ++ i) {
-    h_data[i] = i * (i + 1);
-  }
-
   cudaMalloc((void**)&d_data, size);
-  cudaMalloc((void**)&d_ret, 1 * sizeof(float));
+  cudaMalloc((void**)&d_ret, blockPerGrid * sizeof(float));
 
-  cudaMemcpy(d_data, h_data, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_data, data.begin(), size, cudaMemcpyHostToDevice);
+  
+  arraySum<<<blockPerGrid, threadsPerBlock>>>(d_data, d_ret, numElements);
+  cudaMemcpy(ret.begin(), d_ret, blockPerGrid * sizeof(float), cudaMemcpyDeviceToHost);
 
-  arraySum<<<1, 16>>>(d_data, d_ret, numElements);
-  cudaMemcpy(h_ret, d_ret, 1 * sizeof(float), cudaMemcpyDeviceToHost);
+  int result = std::accumulate(ret.begin(), ret.end(), 0);
 
-  // float temp[16];
-  // cudaMemcpy(temp, d_data, 16 * sizeof(float), cudaMemcpyDeviceToHost);
-
-  std::cout << "result: " << h_ret[0] << std::endl;
+  std::cout << "result: " <<  result << std::endl;
   cudaFree(d_data);
   cudaFree(d_ret);
-  free(h_data);
-  free(h_ret);
   return 0;
 }
